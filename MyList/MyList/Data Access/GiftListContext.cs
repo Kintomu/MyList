924d2ff481f457b2dc104a1e75b5cfa5c1bc96d4
Kintomu <taylorpkeefe@gmail.com>
2024-12-07 15:44:47 -0500

    built the wrong project file, recreated all work in new project, added a page to see and add basic items to the db

---
+++ MyList/MyList/Data Access/GiftListContext.cs
@@ -0,0 +1,39 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using MyList.Models;
+
+namespace MyList.Data_Access;
+
+public class GiftListContext : DbContext
+{
+    public GiftListContext(DbContextOptions<GiftListContext> options) : base(options)
+    {
+    }
+    public DbSet<GiftList> GiftLists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=GiftList.db");
+    }
+
+}
+
+public class DataInitializer : DbCommandInterceptor
+{
+    public override InterceptionResult<int> NonQueryExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<int> result)
+    {
+        Process(command, eventData);
+        return result;
+    }
+
+    private void Process(DbCommand command, CommandEventData eventData)
+    {
+        if (command.CommandText.StartsWith("CREATE TABLE"))
+        {
+            command.CommandText += @";
+                INSERT INTO GiftLists (ListName, Description) VALUES ('Birthday Gifts', 'Gifts for upcoming birthdays');
+                INSERT INTO GiftLists (ListName, Description) VALUES ('Holiday Gifts', 'Presents for the holiday season');";
+        }
+    }
+}
