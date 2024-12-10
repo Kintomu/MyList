0e46ad9b0e82b7a7e72ddb7f7ad8b83ed23c1fcf
Kintomu <taylorpkeefe@gmail.com>
2024-12-10 13:43:36 -0500

    added add item to list

--- MyList/MyList/obj/Debug/net8.0/MyList.AssemblyInfo.cs
+++ MyList/MyList/obj/Debug/net8.0/MyList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f09dd3bf84efe4b89d028eb8a303843d85c5576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1124c348259d0a5f57956150f696d764f73dabf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
